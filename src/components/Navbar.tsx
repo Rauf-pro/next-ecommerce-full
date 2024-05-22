6b312d338609b89b9a184e4b61782898a8f081bd
rrauf8178 <rrauf8178@gmail.com>
2024-05-22 19:58:30 +0400

    add: mobile menu for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,20 @@
-
+import Link from "next/link";
+import Menu from "./Menu";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* MOBILE */}
+      <div className="h-full flex items-center justify-between">
+        <Link href={"/"}>
+          <div className="text-2xl tracking-wide ">Rafa</div>
+        </Link>
+        <Menu />
+      </div>
+      {/* BIGGER SCREENS */}
+      <div className="hidden md:flex items-center justify-between gap-8 h-full"></div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
