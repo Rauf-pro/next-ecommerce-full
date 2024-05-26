c8c2a8d60104bc6df1f10f957388ae28f778b1ca
rrauf8178 <rrauf8178@gmail.com>
2024-05-26 19:00:21 +0400

    add: Add component to SinglePage page

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -12,7 +12,17 @@ const CustomizeProducts = () => {
         </li>
       </ul>
       <h4 className="">Choose a size</h4>
-      <ul className=""></ul>
+      <ul className="flex items-center gap-3">
+      <li className="ring-1 ring-[#F35C7A] text-[#F35C7A] rounded-md py-1 px-4 text-sm cursor-pointer">
+          Small
+        </li> 
+      <li className="ring-1 ring-[#F35C7A] text-white bg-[#F35C7A] rounded-md py-1 px-4 text-sm cursor-pointer">
+          Medium
+        </li>
+        <li className="ring-1 ring-pink-200 text-white bg-pink-200 rounded-md py-1 px-4 text-sm cursor-not-allowed">
+          Large
+        </li>
+      </ul>
     </div>
   );
 };
