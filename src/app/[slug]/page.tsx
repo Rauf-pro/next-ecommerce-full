6e7a48f78716575b95b5dee1f8059d7510619da8
rrauf8178 <rrauf8178@gmail.com>
2024-05-26 17:27:50 +0400

    add: choose color on CustomizeProduct component

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,6 @@
+import Add from "@/components/Add";
+import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductImages from "@/components/ProductImages";
-import React from "react";
 
 const SinglePage = () => {
   return (
@@ -9,7 +10,42 @@ const SinglePage = () => {
         <ProductImages />
       </div>
       {/* TEXTS  */}
-      <div className="w-full lg:w-1/2">TEXTS</div>
+      <div className="w-full lg:w-1/2 flex flex-col gap-6">
+        <h1 className="text-4xl font-medium">Product Name</h1>
+        <p className="text-gray-500">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, natus.
+        </p>
+        <div className="h-[2px] w-full bg-gray-100"></div>
+        <div className="flex items-center gap-4">
+          <h3 className="tex-xl text-gray-500 line-through">$59</h3>
+          <h2 className="font-medium text-2xl">$49</h2>
+        </div>
+        <div className="h-[2px] w-full bg-gray-100"></div>
+        <CustomizeProducts />
+        <Add />
+        <div className="h-[2px] w-full bg-gray-100"></div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Reprehenderit, incidunt? Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Reprehenderit, incidunt? Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+        <div className="text-sm">
+          <h4 className="font-medium mb-4">Title</h4>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Reprehenderit, incidunt? Lorem ipsum dolor sit amet.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
